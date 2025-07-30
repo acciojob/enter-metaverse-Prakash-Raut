c7d0211a757aba2483c7637ca3c62ac478deeeca
Prakash Raut <prakash_raut@outlook.com>
2025-07-30 10:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const stat = document.getElementById("status");
+const btn = document.getElementById("enterBtn");
+
+btn.addEventListener("click", () => {
+	stat.innerHTML = "<h1>Entered Metaverse</h1>";
+});
